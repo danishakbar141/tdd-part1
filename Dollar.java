68189ea086754e566595099aff41aeee005ec4ad
Danish Akbar <daniak141@yahoo.com>
2014-12-29 18:16:20 +0500

    Example 2

--- Dollar.java
+++ Dollar.java
@@ -2,24 +2,24 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package exmp1;
+package example2;
 
 /**
  *
  * @author Danish Akbar
  */
 public class Dollar {
-private int amount;
+   private int amount;
 Dollar(int amount){
 this.amount=amount;
 }
-public void times(int multiplier){
-amount *=multiplier;
+public Dollar times(int multiplier){
+return new Dollar(amount*multiplier);
 }
 public int getamount(){
 return amount;
 }
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+    } 
 }
