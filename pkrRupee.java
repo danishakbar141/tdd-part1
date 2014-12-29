044f090406e958c45f5eb3633d590e88c60164fc
Danish Akbar <daniak141@yahoo.com>
2014-12-29 18:23:13 +0500

    Example 4

--- pkrRupee.java
+++ pkrRupee.java
@@ -8,20 +8,20 @@ package exmp4;
  *
  * @author Danish Akbar
  */
-public class Dollar {
+public class pkrRupee {
 private int amount;
-Dollar(int amount){
+pkrRupee(int amount){
 this.amount=amount;
 }
-public Dollar times(int multiplier){
-return new Dollar(amount*multiplier);
+public pkrRupee times(int multiplier){
+return new pkrRupee(amount*multiplier);
 }
 public int getamount(){
 return amount;
 }
 @Override
 public boolean equals(Object obj){
-    Dollar dol=(Dollar)obj;
+    pkrRupee dol=(pkrRupee)obj;
     return amount==dol.amount;
 }
     public static void main(String[] args) {
